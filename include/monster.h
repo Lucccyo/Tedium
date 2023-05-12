ba6dbe7c002fa7fc0347170a813bc6e22c150bb4
Lucccyo <cha.git@mailo.fr>
2023-05-12 17:52:27 +0200

    add monster file

---
+++ include/monster.h
@@ -0,0 +1,29 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdlib.h>
+
+struct monster {
+  /* health[0] -> current health
+     health[1] -> max health */
+  int health[2];
+  /* stats[0] -> attack damage
+     stats[1] -> defense */
+  int stats[2];
+  int room_id;
+  /* coordinate[0] -> x position in current room
+     coordinate[1] -> y position in current room */
+  int coordinate[2];
+};
+typedef struct monster Monster ;
+
+/* initialize a monster */
+void reset_monster(Monster * m, int room_id, int x, int y);
+
+/* stats functions */
+void update_health(int* health, int update);
+
+/* display debug */
+void display_monster(Monster * m);
+
+#endif
---
+++ src/monster.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "../include/monster.h"
+
+/* initialize a monster */
+void reset_monster(Monster * m, int room_id, int x, int y) {
+  m->health[0] = 10; m->health[1] = 10;
+  m->stats[0] = 2; m->stats[1] = 1;
+  m->room_id = room_id;
+  m->coordinate[0] = x; m->coordinate[1] = y;
+}
+
+/* stats functions */
+void update_health(int* health, int update) { (*health)+=update; }
+
+/* display debug */
+void display_monster(Monster * m) {
+  printf("\n"
+  "health  : (%d/%d)\n"
+  "attack  : %d\n"
+  "defense : %d\n"
+  "room_id : %d\n"
+  "coordinates : (%d;%d)\n\n",
+  m->health[0],
+  m->health[1],
+  m->stats[0],
+  m->stats[1],
+  m->room_id,
+  m->coordinate[0],
+  m->coordinate[1]);
+}
