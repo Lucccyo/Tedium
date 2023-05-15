2c9d9da02680f4d870197df69f6a687da96bdad7
Loïc Pottier <loic.pottier@live.fr>
2023-05-15 13:44:12 +0200

    makefile change, folder changes

--- src/main.c
+++ src/main.c
@@ -1,22 +1,46 @@
-#include "../include/room.h"
-#include "../include/floor.h"
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+#include <../include/SDL2/SDL.h>
 
 int main() {
-    /* room creation test
-    Room room = create_room("rooms/empty.level");
-    display_room(room); */
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
 
-    /* floor creation test */
-    Floor test_floor = create_floor("rooms/level1");
-    display_floor(test_floor);
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Error inititializing SDL: %s\n", SDL_GetError());
+        return 1;
+    }
 
-    printf("Room in y1 | x0 is : %s\n", test_floor.rooms[1][0].name);
-    Room *east_neighbor = test_floor.rooms[1][0].east;
-    printf("east neighbor is : %s\n", east_neighbor->name);
-    /* Seg fault car pas de voisin 
-       A voir comment on gere les salles vides
-       pointeur vers empty.level ? 
-    Room *south_neighbor = test_floor.rooms[1][0].south;
-    printf("south neighbor is : %s\n", south_neighbor->name); */
+    window = SDL_CreateWindow("Tedium", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Error creating window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        printf("Error creating renderer: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Event event;
+    int quit = 0;
+
+    while (!quit) {
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
+                quit = 1;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
---
+++ tests/test_floor.c
@@ -0,0 +1,17 @@
+#include "../include/room.h"
+#include "../include/floor.h"
+
+int main() {
+    /* room creation test */
+    Room room = create_room("rooms/empty.level");
+    display_room(room);
+
+    /* floor creation test */
+    Floor test_floor = create_floor("rooms/level1");
+    display_floor(test_floor);
+
+    printf("Room in y1 | x0 is : %s\n", test_floor.rooms[1][0].name);
+    Room *east_neighbor = test_floor.rooms[1][0].east;
+    printf("east neighbor is : %s\n", east_neighbor->name);
+    return 0;
+}
