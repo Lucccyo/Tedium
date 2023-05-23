9060f9abff6ade9da0e953a55a68f4e431f275fe
Thomas Los <thomas.los@tomorrowtech.fr>
2023-05-23 12:09:32 +0200

    feat: player movement

--- src/main.c
+++ src/main.c
@@ -74,6 +74,10 @@ int main()
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
 
+    Uint32 lastUpdate = SDL_GetTicks();
+
+    int quit = 0;
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         printf("Error inititializing SDL: %s\n", SDL_GetError());
@@ -127,12 +131,31 @@ int main()
     Floor *test_floor = create_floor("maze/floor1/", h);
     Room *target_room = test_floor->rooms[0];
 
+    SDL_Surface *plrimg = SDL_LoadBMP("gfx/player.bmp");
+    SDL_Texture *plrtexture = SDL_CreateTextureFromSurface(renderer, plrimg);
+    SDL_FreeSurface(plrimg);
+
+    Player *player = init_player();
+    player->coordinate[0] = (int)WINDOW_WIDTH / 2;
+    player->coordinate[1] = (int)WINDOW_HEIGHT / 2;
+    int speed = 50;
+
+    int ZDown = 0;
+    int QDown = 0;
+    int SDown = 0;
+    int DDown = 0;
+
     Texture texture = load_textures(renderer);
-    while (!quit)
-    {
+    while (!quit) {
         // physics
-        Uint32 cTime = SDL_GetTicks();
-        float dT = (cTime - lastUpdate) / 500.0f;
+        Uint32 current = SDL_GetTicks();
+        float dT = (current - lastUpdate) / 500.0f;
+
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
+                quit = 1;
+                break;
+            }
 
         while (SDL_PollEvent(&event) != 0)
         {
@@ -142,97 +165,125 @@ int main()
                 clicked = button_clicked(event.button, newButton);
                 if (clicked)
                 {
-                    (*newButton->callback)(1);
-                    current = &newButton->rect;
-                }
-                break;
-            case SDL_MOUSEBUTTONUP:
-                current = NULL;
-                break;
+                case SDLK_LEFT:
+                    if (target_room->neighbors[WEST] != NULL) {
+                        target_room = target_room->neighbors[WEST];
+                    }
+                    break;
+                case SDLK_RIGHT:
+                    if (target_room->neighbors[EAST] != NULL) {
+                        target_room = target_room->neighbors[EAST];
+                    }
+                    break;
+                case SDLK_UP:
+                    if (target_room->neighbors[NORTH] != NULL) {
+                        target_room = target_room->neighbors[NORTH];
+                    }
+                    break;
+                case SDLK_DOWN:
+                    if (target_room->neighbors[SOUTH] != NULL) {
+                        target_room = target_room->neighbors[SOUTH];
+                    }
+                    break;
 
-            case SDLK_LEFT:
-                if (target_room->neighbors[WEST] != NULL)
+                case SDLK_z:
+                    ZDown = 1;
+                    break;
+                case SDLK_q:
+                    QDown = 1;
+                    break;
+                case SDLK_s:
+                    SDown = 1;
+                    break;
+                case SDLK_d:
+                    DDown = 1;
+                    break;
+                default:
+                    break;
+                }
+            case SDL_KEYUP:
+                /* Check the SDLKey values and change room if neighbor exists */
+                switch (event.key.keysym.sym)
                 {
-                    target_room = target_room->neighbors[WEST];
+                case SDLK_z:
+                    ZDown = 0;
+                    break;
+                case SDLK_q:
+                    QDown = 0;
+                    break;
+                case SDLK_s:
+                    SDown = 0;
+                    break;
+                case SDLK_d:
+                    DDown = 0;
+                    break;
+                default:
+                    break;
                 }
-                break;
-            case SDLK_RIGHT:
-                if (target_room->neighbors[EAST] != NULL)
-                {
-                    target_room = target_room->neighbors[EAST];
-                }
-                break;
-            case SDLK_UP:
-                if (target_room->neighbors[NORTH] != NULL)
-                {
-                    target_room = target_room->neighbors[NORTH];
-                }
-                break;
-            case SDLK_DOWN:
-                if (target_room->neighbors[SOUTH] != NULL)
-                {
-                    target_room = target_room->neighbors[SOUTH];
-                }
-                break;
-
-            case SDLK_z:
-                player->coordinate[1] -= speed * dT;
-                if (player->coordinate[1] > 0)
-                    break;
-                player->coordinate[1] = 0;
-                if (target_room->neighbors[NORTH] == NULL)
-                    break;
-
-                target_room = target_room->neighbors[NORTH];
-                player->coordinate[1] = WINDOW_HEIGHT - 32;
-                break;
-            case SDLK_q:
-                player->coordinate[0] -= speed * dT;
-
-                if (player->coordinate[0] > 0)
-                    break;
-
-                player->coordinate[0] = 0;
-                if (target_room->neighbors[WEST] == NULL)
-                    break;
-
-                target_room = target_room->neighbors[WEST];
-                player->coordinate[0] = WINDOW_WIDTH - 32;
-                break;
-            case SDLK_s:
-                player->coordinate[1] += speed * dT;
-                if (player->coordinate[1] + 32 < WINDOW_HEIGHT)
-                    break;
-                player->coordinate[1] = WINDOW_HEIGHT - 32;
-                if (target_room->neighbors[SOUTH] == NULL)
-                    break;
-                
-
-                target_room = target_room->neighbors[SOUTH];
-                player->coordinate[1] = 0;
-                break;
-            case SDLK_d:
-                player->coordinate[0] += speed * dT;
-
-                if (player->coordinate[0] + 32 < WINDOW_WIDTH)
-                    break;
-                player->coordinate[0] = WINDOW_WIDTH - 32;
-                if (target_room->neighbors[EAST] == NULL)
-                    break;
-
-                target_room = target_room->neighbors[EAST];
-                player->coordinate[0] = 0;
-                break;
-
-            case SDL_QUIT:
-                quit = 1;
-                break;
-
             default:
-                continue;
+                break;
             }
         }
 
+        if (ZDown) {
+            player->coordinate[1] -= speed * dT;
+            if (player->coordinate[1] > 0)
+                break;
+            player->coordinate[1] = 0;
+            if (target_room->neighbors[NORTH] == NULL)
+                break;
+
+            target_room = target_room->neighbors[NORTH];
+            player->coordinate[1] = WINDOW_HEIGHT - 32;
+        }
+
+        if (QDown)
+        {
+            player->coordinate[0] -= speed * dT;
+
+            if (player->coordinate[0] > 0)
+                break;
+
+            player->coordinate[0] = 0;
+            if (target_room->neighbors[WEST] == NULL)
+                break;
+
+            target_room = target_room->neighbors[WEST];
+            player->coordinate[0] = WINDOW_WIDTH - 32;
+        }
+
+        if (SDown)
+        {
+            player->coordinate[1] += speed * dT;
+            if (player->coordinate[1] + 32 < WINDOW_HEIGHT)
+                break;
+            player->coordinate[1] = WINDOW_HEIGHT - 32;
+            if (target_room->neighbors[SOUTH] == NULL)
+                break;
+
+            target_room = target_room->neighbors[SOUTH];
+            player->coordinate[1] = 0;
+        }
+
+        if (DDown)
+        {
+            player->coordinate[0] += speed * dT;
+
+            if (player->coordinate[0] + 32 < WINDOW_WIDTH)
+                break;
+            player->coordinate[0] = WINDOW_WIDTH - 32;
+            if (target_room->neighbors[EAST] == NULL)
+                break;
+
+            target_room = target_room->neighbors[EAST];
+            player->coordinate[0] = 0;
+        }
+
+        // physics - Set updated time
+        lastUpdate = current;
+        //object.position += object.velocity * dT;
+
+        // graphics
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
